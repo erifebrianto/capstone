f8fc9405afd54b232ba0509a08c007db4043d733
erifebrianto <eri.febri97@gmail.com>
2025-06-06 19:10:06 +0700

    Update footer.php

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -19,10 +19,6 @@
               2024, made with <i class="fa fa-heart heart text-danger"></i> by
               <a href="http://www.themekita.com">ThemeKita</a>
             </div>
-            <div>
-              Distributed by
-              <a target="_blank" href="https://themewagon.com/">ThemeWagon</a>.
-            </div>
           </div>
         </footer>
       </div>
