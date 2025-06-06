3ead2ea529c8cc7d990265c95cf0c4ba55166b24
erifebrianto <eri.febri97@gmail.com>
2025-06-06 19:32:50 +0700

    update import
    update import

--- application/views/siswa/import.php
+++ application/views/siswa/import.php
@@ -1,9 +1,21 @@
 <div class="container">
-    <h2>Import Data Siswa</h2>
-    <?php if (isset($error)) echo "<div class='alert alert-danger'>$error</div>"; ?>
-    
-    <?= form_open_multipart('siswa/import') ?>
-    <input type="file" name="file_excel" accept=".xls,.xlsx" required>
-    <button type="submit" class="btn btn-primary">Upload</button>
-    </form>
+    <div class="page-inner">
+        <div class="card mt-4">
+            <div class="card-header bg-primary text-white">
+                <h4 class="mb-0">Import Data Siswa</h4>
+            </div>
+            <div class="card-body">
+                <?php if (isset($error)) : ?>
+                    <div class="alert alert-danger"><?= $error ?></div>
+                <?php endif; ?>
+
+                <?= form_open_multipart('siswa/import') ?>
+                    <div class="mb-3">
+                        <input type="file" name="file_excel" accept=".xls,.xlsx" required class="form-control">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Upload</button>
+                </form>
+            </div>
+        </div>
+    </div>
 </div>
